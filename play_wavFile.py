32e76aa75e6033b8901814723e0ab638eb959916
yepi7 <giussepi97@gmail.com>
2024-09-28 19:08:43 +0200

    Arreglado tx y rx en tiempo real por el microfono.

--- play_wavFile.py
+++ play_wavFile.py
@@ -1,12 +1,12 @@
 import socket
 import struct
-import pyaudio
 import audioop
+import pyaudio
 
 # Configuración
 MCAST_GROUP = '239.255.0.1'
 MCAST_PORT = 5004
-CHUNK_SIZE = 1024  # Tamaño del bloque de audio en bytes
+CHUNK_SIZE = 160  # Tamaño de un bloque de audio en bytes
 RATE = 8000  # Tasa de muestreo (8000 Hz)
 
 # Crear un socket UDP para la recepción
@@ -20,19 +20,20 @@ sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
 # Configurar PyAudio para reproducir el audio
 p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paInt16,  # Formato de 16 bits
-                channels=1,  # Audio mono
-                rate=RATE,  # Tasa de muestreo
-                output=True)  # Salida (reproducir)
+stream = p.open(
+    format=pyaudio.paInt16,
+    channels=1,
+    rate=RATE,
+    output=True
+)
 
 def decode_audio(data):
-    # Decodificar el audio de G.711 µ-Law a PCM
+    # Decodifica el audio de G.711 µ-Law a PCM
     return audioop.ulaw2lin(data, 2)
 
 try:
     print("Reproduciendo audio en tiempo real...")
     while True:
-        # Recibir paquete RTP
         data, _ = sock.recvfrom(CHUNK_SIZE + 12)  # 12 bytes para el encabezado RTP
         audio_chunk = data[12:]  # Extraer los datos de audio
         decoded_chunk = decode_audio(audio_chunk)
